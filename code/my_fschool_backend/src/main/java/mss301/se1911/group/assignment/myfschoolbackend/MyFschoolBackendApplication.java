811de96f36f768bebd3ccd3f1a17531e54f700f4
Mr.NoBody <thieuefvanwhieues@gmail.com>
2026-06-01 20:40:35 +0700

    feat: thêm cấu trúc backend bằng spring boot cho dự án

---
+++ code/my_fschool_backend/src/main/java/mss301/se1911/group/assignment/myfschoolbackend/MyFschoolBackendApplication.java
@@ -0,0 +1,13 @@
+package mss301.se1911.group.assignment.myfschoolbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFschoolBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyFschoolBackendApplication.class, args);
+    }
+
+}
