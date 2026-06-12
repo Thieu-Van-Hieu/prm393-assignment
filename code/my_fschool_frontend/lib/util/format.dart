58c64b15a090fd44dc236a90abe1fc87a6e4eddd
Mr.NoBody <thieuefvanwhieues@gmail.com>
2026-06-12 16:04:51 +0700

    feat: thêm util cho việc format thời gian

---
+++ code/my_fschool_frontend/lib/util/format.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+
+String formatTime(String? raw) {
+  if (raw == null || raw.isEmpty) return '--:--';
+  try {
+    DateTime parsedTime;
+
+    if (raw.endsWith('Z')) {
+      String cleanRaw = raw.substring(0, raw.length - 1).replaceAll(' ', 'T');
+      DateTime nativeTime = DateTime.parse(cleanRaw);
+
+      parsedTime = DateTime.parse("$cleanRaw+00:00").toLocal();
+    } else {
+      parsedTime = DateTime.parse(raw).toLocal();
+    }
+
+    if (raw.length >= 16) {}
+
+    String hour = parsedTime.hour.toString().padLeft(2, '0');
+    String minute = parsedTime.minute.toString().padLeft(2, '0');
+
+    return '$hour:$minute';
+  } catch (e) {
+    debugPrint('Lỗi parse giờ: $e');
+    return '--:--';
+  }
+}
